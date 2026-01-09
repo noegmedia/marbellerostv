3347fbb7a462f6f7ff773682ed3a87d1ef0bf0a0
noegmedia <noegmedia@gmail.com>
2026-01-09 11:44:01 +0100

    Refactor script.js to improve time and data loading

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-alert("¡El script funciona!");
-
-
-async function cargarContenido() {
-  try {
-    // Fíjate bien: nombre exacto 'data.json'
-    const res = await fetch('./data.json?v=' + Date.now());
-    const d = await res.json();
-
-    document.getElementById('titulo-principal').innerText = d.titulo;
-    document.getElementById('descripcion-principal').innerText = d.descripcion;
-    document.getElementById('temp').innerText = d.clima;
-    document.getElementById('noticias-scroll').innerText = d.noticias_barra;
-  } catch (e) {
-    console.log("Error de carga");
-  }
+// FUNCIÓN RELOJ
+function reloj() {
+    const elementoReloj = document.getElementById('reloj');
+    if(elementoReloj) {
+        elementoReloj.innerText = new Date().toLocaleTimeString();
+    }
 }
-setInterval(cargarContenido, 5000);
-cargarContenido();
+setInterval(reloj, 1000);
+
+// FUNCIÓN DATOS
+async function cargar() {
+    try {
+        const respuesta = await fetch('./data.json?v=' + Date.now());
+        const d = await respuesta.json();
+        
+        document.getElementById('titulo-principal').innerText = d.titulo;
+        document.getElementById('descripcion-principal').innerText = d.descripcion;
+        document.getElementById('temp').innerText = d.clima;
+        document.getElementById('noticias-scroll').innerText = d.noticias_barra;
+    } catch (e) {
+        console.log("Error");
+    }
+}
+cargar();
+setInterval(cargar, 10000);
